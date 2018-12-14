fa138579fa30ffb9baa4d22aff3749a5e816d505
santi2208 <santi2208@gmail.com>
2018-12-14 01:14:16 -0300

    Agrego Referencia CDN a workbox

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-console.log('sw registered OK!')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
 // // self.importScripts('./node_modules/workbox-sw/build/workbox-sw.js')
 // self.importScripts('./node_modules/workbox-sw/controllers/WorkboxSW.mjs')
 // const staticAssets = [
