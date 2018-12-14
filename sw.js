b4b014f948972985bb9b94c16579a652b9052857
santi2208 <santi2208@gmail.com>
2018-12-13 23:56:47 -0300

    Uso workBox para el SW

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const cacheName = 'statics-v1';
-
+importScripts('./node_modules/workbox-sw/controllers/WorkboxSW.mjs')
 const staticAssets = [
   './',
   './index.js',
@@ -8,24 +7,5 @@ const staticAssets = [
   './audios'
   ];
 
-self.addEventListener('install', async function () {
-  const cache = await caches.open(cacheName);
-  cache.addAll(staticAssets);
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  const request = event.request;
-  const url = new URL(request.url);
-  if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(request));
-  } 
-});
-
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  return cachedResponse || fetch(request);
-}
+  const wb = new WorkboxSW()
+  wb.precache(staticAssets)
