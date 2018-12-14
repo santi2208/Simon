808f6b26b97d3008416b1bc9bb3f2c752aa504fb
santi2208 <santi2208@gmail.com>
2018-12-14 01:17:01 -0300

    workbox referenciaLocal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('./workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
